20616be254f7fd7356ae3c376db8a87b1f935b46
daneren <2598342038@qq.com>
2020-09-07 11:44:19 +0800

    update simple version

--- training/pytorch_qat/v1.4/examples/models/resnet_.py
+++ training/pytorch_qat/v1.4/examples/models/resnet_.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 from torchvision.models.utils import load_state_dict_from_url
 
+import os, sys
+base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(base_path)
+from quantization_wrap.utils import ops
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152', 'resnext50_32x4d', 'resnext101_32x8d',
@@ -53,7 +57,7 @@ class BasicBlock(nn.Module):
         self.downsample = downsample
         self.stride = stride
 
-        self.op = nn.quantized.FloatFunctional()
+        self.add = ops.AddWrap()
 
     def forward(self, x):
         identity = x
@@ -69,7 +73,7 @@ class BasicBlock(nn.Module):
             identity = self.downsample(x)
 
         # out += identity
-        out = self.op.add(out, identity)
+        out = self.add(out, identity)
         out = self.relu(out)
 
         return out
@@ -101,7 +105,7 @@ class Bottleneck(nn.Module):
         self.downsample = downsample
         self.stride = stride
 
-        self.op = nn.quantized.FloatFunctional()
+        self.add = ops.AddWrap()
 
     def forward(self, x):
         identity = x
@@ -121,7 +125,7 @@ class Bottleneck(nn.Module):
             identity = self.downsample(x)
 
         # out += identity
-        out = self.op.add(out, identity)
+        out = self.add(out, identity)
         out = self.relu(out)
 
         return out
@@ -152,7 +156,7 @@ class ResNet(nn.Module):
                                bias=False)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
                                        dilate=replace_stride_with_dilation[0])
@@ -163,12 +167,8 @@ class ResNet(nn.Module):
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
-        self.flatten_dequant = torch.quantization.DeQuantStub()
-        self.flatten_quant = torch.quantization.QuantStub()
-
-        self.maxpool_dequant = torch.quantization.DeQuantStub()
-        self.maxpool_quant = torch.quantization.QuantStub()
-
+        self.maxpool = ops.MaxPoolWrap(kernel_size=3, stride=2, padding=1)
+        self.flatten = ops.FlattenWrap()
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -216,10 +216,7 @@ class ResNet(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-
-        x = self.maxpool_dequant(x)
         x = self.maxpool(x)
-        x = self.maxpool_quant(x)
 
         x = self.layer1(x)
         x = self.layer2(x)
@@ -227,11 +224,7 @@ class ResNet(nn.Module):
         x = self.layer4(x)
 
         x = self.avgpool(x)
-
-        x = self.flatten_dequant(x)
-        x = torch.flatten(x, 1)
-        x = self.flatten_quant(x)
-        
+        x = self.flatten(x)
         x = self.fc(x)
 
         return x
