73de0054663a0e759ba70942e9c9a09d09333584
charlieguo0307 <869425298@qq.com>
2020-09-27 11:36:04 +0800

    [QAT convert]Add onnx to trtengine tools

--- training/tf_v1_qat/convert/tf2trt/README.md
+++ training/tf_v1_qat/convert/tf2trt/README.md
@@ -3,6 +3,11 @@
 ## Description
 This sample demonstrates workflow for inferencing a Resnet-50 model trained using Quantization Aware Training with TensorRT.
 
+## Prerequisites
+ - TensorRT >=7.1
+ - Tensorflow=1.15
+ - cuda >=10.2
+
 ## Running the sample
 
 ### Step 1. Postprocessing checkpoint
@@ -18,3 +23,43 @@ This sample demonstrates workflow for inferencing a Resnet-50 model trained usin
     cd training/tf_v1_qat/convert/tf2trt
     python export_frozen_graph.py --checkpoint=/path/new.ckpt --quantize --input_format NCHW --output_file=/path/resnet50v1.5.pb --symmetric --use_qdq --use_final_conv
     ```
+
+### Step 3. Tensorflow pb to ONNX
+ - Convert tensorflow pb to ONNX. This step needs modifications in Tensorflow graph grappler to disable quantization scales folding into convolution op. You can use <a href="https://ngc.nvidia.com/catalog/containers/nvidia:tensorflow">TensorFlow NGC containers</a> or compile Tensorflow from source with this <a href="https://github.com/NVIDIA/tensorflow/commit/56d0fcb3ebc72c64deeed11e3443dae0a6bbee01#diff-f3c968af33d813270dacefabc01b6a73">commit</a>.
+    ```
+    python3 -m tf2onnx.convert --input /path/resnet50v1.5.pb --output /path/resnet50v1.5.onnx --inputs input:0 --outputs softmax_tensor:0 --opset 11
+    ```
+
+### Step 4. ONNX postprocess
+- Postprocess onnx to remove some transpose nodes.
+    ```
+    cd training/tf_v1_qat/convert/tf2trt
+    python postprocess_onnx.py --input /path/resnet50v1.5.onnx --output /path/postprocessed_rn50.onnx
+    ```
+   
+### Step 5. Build TensorRT engine
+- Build TensorRT engine using python API.
+    ```
+    cd training/tf_v1_qat/convert/tf2trt
+    python build_engine.py --onnx /path/postprocessed_rn50.onnx --engine /path/rn50qat.trt
+    ```
+
+### Step 6. Validation
+- Validation on a single image using python API.
+    ```
+    cd training/tf_v1_qat/convert/tf2trt
+    python infer.py --engine /path/rn50qat.trt --image ILSVRC2012_val_00034812.JPEG
+    ```
+- The output is like this
+    ```
+    Output class of the image: marmot Confidence: 0.6862110495567322
+    ```
+
+## Roadmap
+- Per-channel quantization
+- Per-layer quantization
+- Supporting more models
+- ...
+
+## References
+- Nvidia TensorRT <a href="https://github.com/NVIDIA/sampleQAT">sampleQAT</a> tools
---
+++ training/tf_v1_qat/convert/tf2trt/build_engine.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# Copyright 2020 NVIDIA Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import tensorrt as trt
+
+
+def build_profile(builder, network, profile_shapes):
+    """
+    Build optimization profile for the builder and configure the min, opt, max shapes appropriately.
+    """
+
+    def is_dimension_dynamic(dim):
+        return dim is None or dim <= 0
+
+    def override_shape(shape):
+        return tuple([1 if is_dimension_dynamic(dim) else dim for dim in shape])
+
+    profile = builder.create_optimization_profile()
+    for idx in range(network.num_inputs):
+        inp = network.get_input(idx)
+
+        def get_profile_shape(name):
+            if name not in profile_shapes:
+                return None
+            shapes = profile_shapes[name]
+            return shapes
+
+        if inp.is_shape_tensor:
+            shapes = get_profile_shape(inp.name)
+            min, opt, max = shapes
+            profile.set_shape_input(inp.name, min, opt, max)
+            print("Setting shape input: {:} values to min: {:}, opt: {:}, max: {:}".format(inp.name, min, opt, max))
+        elif -1 in inp.shape:
+            shapes = get_profile_shape(inp.name)
+            if not shapes:
+                shapes = [override_shape(inp.shape)] * 3
+                print(
+                    "Overriding dynamic input shape {:} to {:}. If this is incorrect, "
+                    "for input tensor: {:}, please provide tuples for min, opt, "
+                    "and max shapes containing values: {:} with dynamic dimensions replaced,".format(
+                        inp.shape, shapes[0], inp.name, inp.shape))
+            min, opt, max = shapes
+            profile.set_shape(inp.name, min, opt, max)
+            print("Setting input: {:} shape to min: {:}, opt: {:}, max: {:}".format(inp.name, min, opt, max))
+    if not profile:
+        print("Profile is not valid, please provide profile data. Note: profile was: {:}".format(profile_shapes))
+    return profile
+
+
+def build_engine_onnx(model_file, verbose=False):
+    """
+    Parse the model file through TensorRT, build TRT engine and run inference
+    """
+    # Create builder and network
+    if verbose:
+        TRT_LOGGER = trt.Logger(trt.Logger.VERBOSE)
+    else:
+        TRT_LOGGER = trt.Logger(trt.Logger.INFO)
+
+    network_flags = 1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+    network_flags = network_flags | (1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_PRECISION))
+
+    with trt.Builder(TRT_LOGGER) as builder, builder.create_network(flags=network_flags) as network, trt.OnnxParser(
+            network, TRT_LOGGER) as parser:
+        with open(model_file, 'rb') as model:
+            if not parser.parse(model.read()):
+                print('ERROR: Failed to parse the ONNX file.')
+                for error in range(parser.num_errors):
+                    print(parser.get_error(error))
+                return None
+
+        config = builder.create_builder_config()
+        config.max_workspace_size = 1 << 30
+        config.flags = config.flags | 1 << int(trt.BuilderFlag.INT8)
+        # Setting the (min, opt, max) batch sizes to be 1. Users need to configure this according to their requirements.
+        config.add_optimization_profile(build_profile(builder, network, profile_shapes={
+            'input': [(1, 3, 224, 224), (1, 3, 224, 224), (1, 3, 224, 224)]}))
+
+        return builder.build_engine(network, config)
+
+
+def main(args):
+    model_file = args.onnx
+    # Parse the ONNX graph through TensorRT and build the engine
+    trt_engine = build_engine_onnx(model_file, args.verbose)
+    # Serialize the engine and save to file
+    with open(args.engine, "wb") as file:
+        file.write(trt_engine.serialize())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--onnx", type=str, default='rn50.onnx', help="Path to RN50 ONNX graph")
+    parser.add_argument("--engine", type=str, default='rn50_trt.engine', help="output path to TensorRT engine")
+    parser.add_argument('-v', '--verbose', action='store_true', help="Flag to enable verbose logging")
+    args = parser.parse_args()
+    main(args)
